902c0c44d7e09c9dea9fb8703c1852106745c986
Emily Yang <emilyyang@Emilys-MacBook-Air.local>
2019-07-15 15:43:48 -0700

    "finished json"

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -1,18 +1,21 @@
 // test cases are described in fixtures.js
 describe('stringifyJSON', function() {
   it('should match the result of calling JSON.stringify', function() {
+// var expect = chai.expect;
 
     stringifiableObjects.forEach(function(test) {
       var expected = JSON.stringify(test);
       var result = stringifyJSON(test);
-      expect(result).to.equal(FILL_ME_IN);
+      expect(result).to.equal(expected);
     });
 
     unstringifiableValues.forEach(function(obj) {
       var expected = JSON.stringify(obj);
       var result = stringifyJSON(obj);
-      expect(result).to.equal(FILL_ME_IN);
+      expect(result).to.equal(expected);
     });
-
   });
 });
+
+// var test = { x: 5, y: 6 };
+// var obj = [null, undefined, 2, false, "hi"]
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,70 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+//input:object or array or value
+//output: string
+  //if typeof obj is undefined or symbol or null or function
+    // return "null"
+  //if type of obj is boolean or number
+    // return obj without quotes
+  //if type of obj is string
+    // return obj with quotes
+  // if obj !isArray or is an obj
+    //create a result variable = "{"
+    //loop through for..in 
+      //result += stringifyJSON(obj[key])
+  // if obj isArray
+  //create a result variable = "["
+    //loop through with for loop 
+      //result += stringifyJSON(obj[i])
+  //return result
+  if (typeof obj === "symbol" || obj === null) {
+    return "null";
+  }
+  else if (typeof obj === "function" || obj === undefined ) {
+    return "";
+  }
+  else if (typeof obj === "boolean" || typeof obj === "number") {
+    return `${obj}`;
+  }
+  else if (typeof obj === "string") {
+    return `"${obj}"`;
+  }
+  else if (typeof obj === "object") {
+    if (!Array.isArray(obj)) { //if it is an object
+      var output = `{`;
+      if (Object.keys(obj).length === 0 && obj.constructor === Object) {
+        return "{}";
+      }
+
+      for (var key in obj) {
+        if (typeof obj[key] === "function" || obj[key] === undefined || key === undefined) {
+          return "{}";
+        }
+        output += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+      }
+        return output.slice(0,-1) + `}`;
+    }
+    else if (Array.isArray(obj)) { //if it is an array
+      var output = `[`;
+      if (obj.length === 0) {
+          return "[]";
+      }
+    for (var i = 0; i < obj.length; i++) {
+       output += stringifyJSON(obj[i]) + ",";
+    }
+    return output.slice(0,-1) + `]`;
+    }
+  }
+  return output;
 };
+
+stringifyJSON({ x: 5, y: 6 });
+// console.log(stringifyJSON({ x: 5, y: 6 }));
+// // expected output: "{"x":5,"y":6}"
+// // "{"x":5,"y":6
+// var test = { x: 5, y: 6 };
+// var obj = [null, undefined, 2, false, "hi"]
+
+stringifyJSON([null, undefined, 2, false, "hi"]);
+// // expected output: "[null,null,2,false,"hi"]"
